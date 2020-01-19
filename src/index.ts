12ba23cc079f7b31245932ab77f75f610e44155f
rkterungwa16 <terungwakombol@gmail.com>
2020-01-19 19:44:21 +0100

    setup websockets for application

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import * as dotenv from "dotenv";
 import { createAppLogger } from "./middlewares";
 import { init } from "./server";
+import { socketSetup } from "./services/socket"
+import { initSocketServer } from "./socketServer";
 
 dotenv.config();
+const webSocketServer = initSocketServer(init());
+socketSetup(webSocketServer);
 init().listen(3300);
 createAppLogger("Task Manager Start up").log({
     level: "info",
