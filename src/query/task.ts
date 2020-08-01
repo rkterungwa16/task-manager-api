a632a5c9c21728a74f32f160ee1d76636d8f8c75
rkterungwa16 <terungwakombol@gmail.com>
2020-08-01 23:37:06 +0100

    [feat] add project task query

---
+++ src/query/task.ts
@@ -0,0 +1,23 @@
+import { ObjectId } from "mongodb";
+export const listProjectTasks = (id: ObjectId) => {
+    return [
+        {
+            $match: {
+                _id: id
+            }
+        },
+        {
+            $lookup: {
+                from: "projects",
+                localField: "project",
+                foreignField: "_id",
+                as: "project"
+            }
+        },
+        {
+            $unwind: {
+                path: "$project"
+            }
+        }
+    ];
+};
